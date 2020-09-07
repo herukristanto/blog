40acc2c2c0102fdb88cbbe3bb9c84dea3e76e72c
herukristanto <herukristanto90@gmail.com>
2020-09-07 08:00:05 +0700

    slicing login and register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './register.scss';
+import { RegisterBg } from '../../assets';
+import { Input, Button, Gap, Link } from '../../components';
 
-const Registre = () => {
+
+const Register = () => {
     return (
-        <div>
-            <p>Page Register </p>
+        <div className="main-page">
+            <div className="left">
+                <img src={RegisterBg} className="bg-image" alt="imageBG" />
+            </div>
+            <div className="right">
+                <p className="title">Register</p>
+                <Input label="Full name" placeholder="Full Name" />
+                <Gap height={15} />
+                <Input label="Email" placeholder="Email" />
+                <Gap height={15} />
+                <Input label="Password" placeholder="Password" />
+                <Gap height={30} />
+                <Button title="Register" />
+                <Gap height={90} />
+                <Link title="Kembali ke login" />
+            </div>
         </div>
     )
 }
 
-export default Registre
+export default Register
